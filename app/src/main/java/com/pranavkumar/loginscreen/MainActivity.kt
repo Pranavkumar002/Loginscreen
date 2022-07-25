0494ec8dbf20a9723730d1bb2e95a29aaa2b84e7
Pranavkumar002 <pranav2002kashyap@gmail.com>
2022-07-25 12:55:38 +0530

    Forget password

--- app/src/main/java/com/pranavkumar/loginscreen/MainActivity.kt
+++ app/src/main/java/com/pranavkumar/loginscreen/MainActivity.kt
@@ -10,7 +10,8 @@ import androidx.core.widget.doOnTextChanged
 class MainActivity : AppCompatActivity() {
     lateinit var etEmail: EditText
     lateinit var etPassword: EditText
-    lateinit var tvForgot: TextView
+    lateinit var btnForgot: TextView
+    lateinit var btnRegister: Button
     lateinit var btnLogin: Button
     lateinit var ivGoogle: ImageView
     lateinit var ivFacebook: ImageView
@@ -22,11 +23,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.login_main)
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
-        tvForgot = findViewById(R.id.tvForgot)
+        btnForgot = findViewById(R.id.btnForgot)
+        btnRegister = findViewById(R.id.btnRegister)
         btnLogin = findViewById(R.id.btnLogin)
         ivGoogle = findViewById(R.id.ivGoogle)
         ivFacebook = findViewById(R.id.ivFacebook)
         ivTwitter = findViewById(R.id.ivTwitter)
+
+        btnRegister.setOnClickListener{
+            var intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        btnForgot.setOnClickListener{
+            var intent =Intent(this,ForgotPasswordActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         etPassword.doOnTextChanged { text, _, _, _ ->
             if((text?.length ?:0) <6){
@@ -43,10 +56,10 @@ class MainActivity : AppCompatActivity() {
       System.out.println("clicked")
       var email = etEmail.text.toString()
       var password = etPassword.text.toString()
-      System.out.println("username $email")
+      System.out.println("email $email")
       if (email.isNullOrEmpty())
       {
-          etEmail.error = resources.getString(R.string.please_enter_username)
+          etEmail.error = resources.getString(R.string.please_enter_email)
           etEmail.requestFocus()
       }
       else if (password.isNullOrEmpty())
@@ -57,9 +70,6 @@ class MainActivity : AppCompatActivity() {
       else
       {
           Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_LONG).show()
-          var intent = Intent(this, RegisterActivity::class.java)
-          startActivity(intent)
-          finish()
       }
   }
   ivGoogle.setOnClickListener{
--- app/src/main/java/com/pranavkumar/loginscreen/RegisterActivity.kt
+++ app/src/main/java/com/pranavkumar/loginscreen/RegisterActivity.kt
@@ -14,7 +14,7 @@ class RegisterActivity : AppCompatActivity() {
     lateinit var rgGender: RadioGroup
     lateinit var rbMale: RadioButton
     lateinit var rbFemale: RadioButton
-    lateinit var rbOther: RadioButton
+    lateinit var rbTransgender: RadioButton
     lateinit var btnSubmit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +26,10 @@ class RegisterActivity : AppCompatActivity() {
         etSetPassword = findViewById(R.id.etSetPassword)
         rbMale = findViewById(R.id.rbMale)
         rbFemale = findViewById(R.id.rbFemale)
-        rbOther = findViewById(R.id.rbOther)
+        rbTransgender = findViewById(R.id.rbTransgender)
         rgGender = findViewById(R.id.rgGender)
         btnSubmit = findViewById(R.id.btnSubmit)
+
 
         btnSubmit.setOnClickListener(){
             Toast.makeText(this, resources.getString(R.string.submit_successfully), Toast.LENGTH_LONG).show()
